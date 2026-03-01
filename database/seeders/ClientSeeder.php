c4a148996ea2bc458916cf7f424bcacef8c84ffe
aghafforov <akmal.ghafforov@gmail.com>
2026-03-02 03:43:18 +0500

    Remove PostgreSQL service and volume from Docker Compose and client seeding data from `ClientSeeder.php`.

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -12,8 +12,5 @@ class ClientSeeder extends Seeder
      */
     public function run(): void
     {
-        Client::create(['name' => 'John Doe', 'phone' => '123-456-7890']);
-        Client::create(['name' => 'Jane Smith', 'phone' => '098-765-4321']);
-        Client::create(['name' => 'Acme Corp', 'phone' => '555-555-5555']);
     }
 }
