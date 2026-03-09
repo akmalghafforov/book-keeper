3d1e574ec40c2f2854bc0435298fb7b45a745bf1
aghafforov <akmal.ghafforov@gmail.com>
2026-03-09 23:09:24 +0500

    fix: standardize ledger created_at date format to DD/MM/YYYY in single client debt report.

--- resources/views/admin/reports/pdf/single-client-debt.blade.php
+++ resources/views/admin/reports/pdf/single-client-debt.blade.php
@@ -37,7 +37,7 @@
         <tbody>
             @foreach ($client->recentLedgers as $ledger)
                 <tr>
-                    <td>{{ $ledger?->distribution?->distribution_date?->format('d/m/Y') ?? $ledger->created_at->format('Y-m-d H:i') }}</td>
+                    <td>{{ $ledger?->distribution?->distribution_date?->format('d/m/Y') ?? $ledger->created_at->format('d/m/Y') }}</td>
                     <td>{{ __($ledger->type) }}</td>
                     <td>
                         @if($ledger->distribution)
